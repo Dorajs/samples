a4e38ff41d023a882d8ed996c8f6280075f4dcda
Lin Zhang <linroid@gmail.com>
2019-12-05 19:26:18 +0800

    Update to GitHub organization owner

--- api_demo/components/types/image.js
+++ api_demo/components/types/image.js
@@ -2,10 +2,10 @@ module.exports = {
   type: 'image',
   fetch() {
     return {
-      url: 'https://raw.githubusercontent.com/linroid/dora-vscode/master/docs/explorer.png',
+      url: 'https://raw.githubusercontent.com/DoraKit/vscode-extension/master/docs/explorer.png',
       summary: 'Dora 的 VSCode 开发插件',
       title: 'Dora vscode extension',
-      link: 'https://github.com/linroid/dora-vscode',
+      link: 'https://github.com/DoraKit/vscode-extension',
       author: {
         name: 'linroid',
         avatar: 'https://avatars0.githubusercontent.com/u/3192142?s=460&v=4',

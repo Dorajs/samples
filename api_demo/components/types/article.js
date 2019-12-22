7629b995800201f6ac89ed1f0a45cf5b56d6d2eb
Lin Zhang <linroid@gmail.com>
2019-12-22 21:00:17 +0800

    api_demo: Fix article sample file url

--- api_demo/components/types/article.js
+++ api_demo/components/types/article.js
@@ -3,7 +3,7 @@ module.exports = {
   title: '欢迎使用 Dora.js，官网：https://dorajs.com/',
   thumb: 'https://www.wangbase.com/blogimg/asset/201909/bg2019091201.jpg',
   async fetch() {
-    let resp = await $http.get('https://dorajs.com/README.md')
+    const resp = await $http.get('https://docs.dorajs.com/README.md')
     return {
       content: {
         markdown: resp.data,

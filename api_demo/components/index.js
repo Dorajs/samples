a3341d73e7e6b963aa494fb76d77ac8b09d1101b
Lin Zhang <linroid@gmail.com>
2019-12-23 20:10:20 +0800

    Add prettier config files

--- api_demo/components/index.js
+++ api_demo/components/index.js
@@ -1,23 +1,28 @@
 module.exports = {
   type: 'bottomTab',
   title: 'API Demo',
-  fetch() {
-    return [{
-      title: '组件',
-      route: $route('component'),
-      thumb: $assets('component.svg')
-    }, {
-      title: '全局函数',
-      route: $route('global'),
-      thumb: $assets('global.svg')
-    }, {
-      title: 'icons',
-      route: $route('icons'),
-      thumb: $icon('mood')
-    }, {
-      title: '更多',
-      route: $route('more'),
-      thumb: $icon('more_vert')
-    }]
+  async fetch({ args, page }) {
+    return [
+      {
+        title: '组件',
+        route: $route('component'),
+        thumb: $assets('component.svg')
+      },
+      {
+        title: '全局函数',
+        route: $route('global'),
+        thumb: $assets('global.svg')
+      },
+      {
+        title: 'icons',
+        route: $route('icons'),
+        thumb: $icon('mood')
+      },
+      {
+        title: '更多',
+        route: $route('more'),
+        thumb: $icon('more_vert')
+      }
+    ]
   }
 }

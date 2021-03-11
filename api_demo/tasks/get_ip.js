0279ad0973052a8f14d1dd2146bddbea5a59a3a1
Lin Zhang <linroid@gmail.com>
2021-03-12 01:05:26 +0800

    fill repository infomation in package.json

--- api_demo/tasks/get_ip.js
+++ api_demo/tasks/get_ip.js
@@ -1,4 +1,7 @@
 module.exports = async () => {
   let resp = await $axios.get('https://api.ipify.org?format=json')
-  $ui.alert(`Got IP: ${resp.data.ip}`)
+  return {
+    message: `外网 IP: ${resp.data.ip}`,
+    succeed: true
+  }
 }
